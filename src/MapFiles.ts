979e8b0efbda85def9c00bd85cdb330d922e7f68
Jacob Alford <jacob.alford@me.com>
2023-11-20 17:05:13 -0700

    add source-map schemas

---
+++ src/MapFiles.ts
@@ -0,0 +1,23 @@
+import * as S from 'schemata-ts/index'
+import type * as TCE from 'schemata-ts/TranscodeError'
+import { deriveTranscoderPar } from 'schemata-ts/TranscoderPar'
+
+export const MapFileSchema = S.ParseJsonString(
+  S.Struct(
+    {
+      sources: S.Array(S.String()),
+    },
+    S.Unknown,
+  ),
+)
+
+export type MapFile = S.TypeOf<typeof MapFileSchema>
+
+export const TranscoderPar = deriveTranscoderPar(MapFileSchema)
+
+export class MapFileReadError extends Error {
+  override readonly name = 'MapFileReadError'
+  constructor(readonly transcodeErrors: TCE.TranscodeErrors) {
+    super(String(transcodeErrors))
+  }
+}
